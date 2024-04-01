09fcfbb37df08ec2ecfccca2752a29a78852aadc
jiacy-jcy <714897623@qq.com>
2024-04-01 16:59:43 +0800

    添加示例程序

---
+++ python/calculator/util.py
@@ -0,0 +1,19 @@
+
+
+
+
+class Operation:
+    
+    def Addition(self,param_a,param_b):
+        '''
+        '''
+        pass
+    
+    def subtraction(self,param_a,param_b):
+        pass
+    
+    def multiplication(self,param_a,param_b):
+        pass
+    
+    def division(self,param_a,param_b):
+        pass
